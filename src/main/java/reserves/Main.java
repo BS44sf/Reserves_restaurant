3847729714a7d4efa81f7ae35ca4e03b149e21f8
Abdearg <abdellahar19@gmail.com>
2025-03-07 15:49:48 +0100

    url base dades

--- src/main/java/reserves/Main.java
+++ src/main/java/reserves/Main.java
@@ -1,6 +1,9 @@
 package reserves;
 
 public class Main {
+    static final String DB_URL = "jdbc:mysql://localhost/";
+    static final String USER = "root";
+    static final String PASS = "";
     public static void main(String[] args) {
 //comentari
     }
