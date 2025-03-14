c82c25b437936b84b11bad5bc6a02dc09cbad770
BS44sf <suarezbairon37@gmail.com>
2025-03-14 15:57:25 +0100

    url base dades

--- src/main/java/reserves/Main.java
+++ src/main/java/reserves/Main.java
@@ -1,10 +1,10 @@
 package reserves;
 
 public class Main {
-    static final String DB_URL = "jdbc:mysql://localhost/";
+    static final String DB_URL = "jdbc:mysql://localhost/gestio reserves";
     static final String USER = "root";
     static final String PASS = "";
     public static void main(String[] args) {
-//comentari
+
     }
 }
