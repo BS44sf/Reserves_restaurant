ebf18b9af770fe835bfd9bdf7dacf168b4c6fea4
BS44sf <suarezbairon37@gmail.com>
2025-03-20 17:56:12 +0100

    corrección comprobacion admins-clients

--- src/main/java/reserves/Main.java
+++ src/main/java/reserves/Main.java
@@ -26,8 +26,7 @@ public class Main {
         ResultSet resultadoClientes = sentenciaClientes.executeQuery();
 
         if (resultadoClientes.next()) {
-            System.out.println("Bienvenido, cliente.");
-            client();
+            client(dni);
             // Redirigir al apartado de clientes
         } else {
             String consultaAdmins = "SELECT * FROM admins WHERE dni = ?";
@@ -36,18 +35,41 @@ public class Main {
             ResultSet resultadoAdmins = sentenciaAdmins.executeQuery();
 
             if (resultadoAdmins.next()) {
-                System.out.println("Bienvenido, administrador.");
                 admin();
                 // Redirigir al apartado de administradores
             } else {
-                System.out.println("DNI no encontrado.");
+                System.out.println("DNI no encontrado. ¿Desea registrarse?.");
             }
         }
     }
 
-    public void client() throws SQLException {
+    public void client(String dni) throws SQLException {
         Scanner lector = new Scanner(System.in);
 
+        String opcio;
+        do {
+            System.out.println("Benvingut, seleccioneu una de les opcions:");
+            System.out.println("1. Realitzar una reserva.");
+            System.out.println("2. Realitzar una comanda.");
+            System.out.println("3. Veure el menú.");
+            System.out.println("4. Sortir.");
+            opcio = lector.nextLine();
+
+            switch (opcio) {
+                case "1":
+                    Connection conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+                    System.out.println("Aqui puedes ver las mesas disponibles.");
+                    String taulasDisp = "SELECT * FROM taules WHERE ocupada = false";
+                    PreparedStatement stmt17 = conexion.prepareStatement(taulasDisp);
+                    ResultSet result = stmt17.executeQuery();
+                    if (result.next()) {
+                        System.out.print(" Número de taula: " + result.getInt("id_taula"));
+                        System.out.println(" Capacitat: " + result.getInt("capacitat"));
+                    }
+
+
+            }
+        }while(!opcio.equals("4"));
     }
 
     public void admin() throws SQLException {
